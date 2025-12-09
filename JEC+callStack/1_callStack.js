e40e3d1e84a265a6a0f0b091993ae2a2129ca76f
Vivek Tomer <vivektomer9004@gmail.com>
2025-12-09 08:53:11 +0000

    done

---
+++ JEC+callStack/1_callStack.js
@@ -0,0 +1,8 @@
+// javascript execution context
+
+//1. {} global execution phase context done by 'this'
+// function execution context
+// eval execution context
+
+//2. {} -> memory creation phase
+//3.    -> execution phase
